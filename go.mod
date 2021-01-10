3209bf92cbfd430a8e706a14813f26023aef77f5
Tiago Ilieve <tiago.myhro@gmail.com>
2021-01-09 22:35:21 -0300

    First working version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/myhro/prometheus-ping-exporter
+
+go 1.15
+
+require (
+	github.com/go-ping/ping v0.0.0-20201115131931-3300c582a663
+	github.com/prometheus/client_golang v1.9.0
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-ping/ping"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func checkLoss(packetLoss prometheus.Gauge, target string) {
+	for {
+		time.Sleep(time.Second)
+
+		pinger, err := ping.NewPinger(target)
+		if err != nil {
+			log.Print("ping.NewPinger error: ", err)
+			continue
+		}
+
+		pinger.Timeout = time.Second
+		err = pinger.Run()
+		if err != nil {
+			log.Print("pinger.Run error: ", err)
+			continue
+		}
+
+		stats := pinger.Statistics()
+		packetLoss.Set(stats.PacketLoss)
+	}
+}
+
+func main() {
+	registry := prometheus.NewRegistry()
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	http.Handle("/metrics", handler)
+
+	target := os.Getenv("TARGET_HOST")
+	if target == "" {
+		log.Fatal("Missing environment variable: TARGET_HOST")
+	}
+
+	packetLoss := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "packet_loss",
+		Help: "Number of lost packets",
+	})
+	registry.MustRegister(packetLoss)
+	go checkLoss(packetLoss, target)
+
+	log.Print("Starting server")
+	http.ListenAndServe(":2112", nil)
+}
